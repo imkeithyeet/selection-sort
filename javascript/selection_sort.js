0ff132c5f8fd434e3c645f8cccb892b3003e0d77
Keith Ma <keithchezzy@gmail.com>
2022-12-27 18:32:10 -0800

    sort

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,7 +1,20 @@
 function selectionSort(arr) {
   // type your code here
+    for (let i = 0; i < arr.length -1; i++) {
+      let min = i;
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[min]) {
+          min = j;
+        }
+      }
+      [arr[i],arr[min]] = [arr[min],arr[i]]
+      // * longer alternative way * 
+      // const temp = arr[i];
+      // arr[i] = arr[min];
+      // arr[min] = temp;
+    }
+    return arr;
 }
-
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
@@ -20,4 +33,8 @@ if (require.main === module) {
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+// for loop method first 
+// make a variable of min as i
+// for j method and state i as a min
+// state arr of i and min to equal min and i in reverse order
 // And a written explanation of your solution
